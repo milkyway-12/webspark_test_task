77dbfedaac33056a3d23614ea60bf394d834f008
milkyway-12 <imolokova322@gmail.com>
2025-02-28 21:18:01 +0200

    created api client

---
+++ lib/data/data_sources/remote/api_client.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../local/preferences_helper.dart';
+
+class ApiClient {
+  // final String? baseUrl = PreferencesHelper.instance.getServerUrl();
+  final baseUrl = 'https://flutter.webspark.dev/flutter/api';
+
+  Future<Map<String, dynamic>> getTaskList() async {
+    final response = await http.get(Uri.parse(baseUrl));
+    debugPrint('Response: ${response.body}');
+
+    if (response.statusCode == 200) {
+      debugPrint('Status code = 200, success');
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load matrix data');
+    }
+  }
+
+  Future<void> sendSolutionsList(Map<String, dynamic> data) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/data'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(data),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to post data');
+    }
+  }
+}
