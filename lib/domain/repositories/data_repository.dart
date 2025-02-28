45d119d1a565b3d4ce0fcf7e4af027f5a649a49f
milkyway-12 <imolokova322@gmail.com>
2025-03-01 00:14:45 +0200

    updated DataRepository and created DataRepositoryImpl

--- lib/domain/repositories/data_repository.dart
+++ lib/domain/repositories/data_repository.dart
@@ -1,6 +1,8 @@
+import 'package:webspark_test_task/data/models/response_on_solution.dart';
+import '../../data/models/solution.dart';
 import '../../data/models/task.dart';
 
 abstract class DataRepository {
-  Future<Task> getTaskList();
-  Future<void> sendSolutionsList(Task solution);
+  Future<List<Task>> getTaskList();
+  Future<ResponseOnSolution> sendSolutionsList(List<Solution> solutionsList);
 }
