a436397182e434607e22faeaa7e93348493eb168
milkyway-12 <imolokova322@gmail.com>
2025-03-01 11:30:36 +0200

    added diagonals to move

--- lib/domain/services/direction.dart
+++ lib/domain/services/direction.dart
@@ -2,7 +2,13 @@ enum Direction {
   left(-1, 0),
   right(1, 0),
   top(0, -1),
-  bottom(0, 1);
+  bottom(0, 1),
+
+  // diagonals
+  topLeft(-1, -1),
+  topRight(1, -1),
+  bottomLeft(-1, 1),
+  bottomRight(1, 1);
 
   final int dx;
   final int dy;
