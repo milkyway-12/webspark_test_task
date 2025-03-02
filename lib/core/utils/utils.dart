858a96550d0ccc82a24615c4d401765685ebdd00
milkyway-12 <imolokova322@gmail.com>
2025-03-03 00:58:30 +0200

    added util for parsing points list into string

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,6 +1,11 @@
 import 'dart:ui';
+import '../../data/models/game_point.dart';
 
 Color hexToColor(String hex) {
   hex = hex.replaceAll("#", "");
   return Color(int.parse("0xFF$hex"));
+}
+
+String formatPointsPathToString(List<GamePoint> path) {
+  return path.map((point) => "(${point.y},${point.x})").join("->");
 }
